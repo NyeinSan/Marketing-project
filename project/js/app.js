d06fc5e5c056709f316cb8249184223336021265
Nyein Thu San <85493023+NyeinSan@users.noreply.github.com>
2021-10-27 19:21:08 +0630

    Add files via upload

---
+++ project/js/app.js
@@ -0,0 +1,119 @@
+$(document).ready(function(){
+
+    let screenHeight = $(window).height();
+    console.log(screenHeight);
+    $(window).scroll(function(){
+        let currentPosition = $(this).scrollTop();
+        console.log(currentPosition);
+        if(currentPosition>screenHeight-200){
+            $(".site-nav").addClass("site-nav-scroll");
+        }else{
+            $(".site-nav").removeClass("site-nav-scroll");
+            setActive("home");
+        }
+    });
+
+    $(".navbar-toggler").click(function(){
+        let result = $(".navbar-collapse").hasClass("show");
+        if(result){
+            $(".menu-icon").removeClass("fa-close").addClass("fa-bars")
+        }else{
+            $(".menu-icon").removeClass("fa-bars").addClass("fa-close")
+        }
+    });
+
+    function setActive(current) {
+
+      $(".nav-link").removeClass("current-section");
+
+      $(`.nav-link[href='#${current}']`).addClass('current-section');
+
+  }
+
+  function navScroll() {
+
+      let currentSection = $("section[id]");
+      currentSection.waypoint(function (direction) {
+
+          if(direction == "down"){
+              let currentSectionId = $(this.element).attr('id');
+              console.log(currentSectionId);
+              setActive(currentSectionId);
+          }
+
+      },{ offset:'150px' });
+
+      currentSection.waypoint(function (direction) {
+
+          if(direction == "up"){
+              let currentSectionId = $(this.element).attr('id');
+              console.log(currentSectionId);
+              setActive(currentSectionId);
+          }
+
+      },{ offset:'150px' });
+
+  }
+
+  navScroll();
+
+
+
+    new WOW().init();
+    wow = new WOW(
+                    {
+                    boxClass:     'wow',      // default
+                    animateClass: 'animate__animated', // default
+                    offset:       0,          // default
+                    mobile:       true,       // default
+                    live:         true        // default
+                  }
+                  )
+                  wow.init();
+    $('.pricing-slide').slick({
+      arrows:false,
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+
+   $(window).on("load",function(){
+     $('.loader-container').fadeOut(500,function(){
+      $(this).remove();
+     });
+   });
+
+
+
+
+});
